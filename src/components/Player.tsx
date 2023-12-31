121199dac5840fca3568780169decf77471a85ea
Seemanta Sarker <91196514+StrangerSeemanta@users.noreply.github.com>
2024-01-01 00:58:23 +0600

    Player Updated

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,4 +1,4 @@
-import { Button, Slider, SliderValue } from "@nextui-org/react";
+import { Button, CircularProgress, Slider, SliderValue } from "@nextui-org/react";
 import { Fragment, useEffect, useRef, useState } from "react";
 import getVideoDuration from "../modules/getVideoDuration";
 interface Props {
@@ -16,6 +16,7 @@ function Player({ poster, src }: Props) {
     const [length, setLength] = useState(0);
     const [hideControls, setHideControls] = useState(true)
     const [fullScr, setFullScr] = useState(false);
+    const [isVideoLoading, setVideoLoading] = useState(false)
     const showControlsAutoHide = () => {
         setHideControls(false)
 
@@ -204,12 +205,17 @@ function Player({ poster, src }: Props) {
                         </div>
                     </div>
                 </div>
-                <video onContextMenu={(event) => { event.preventDefault(); }} onDoubleClick={() => setFullScr(!fullScr)} onClick={() => { setPlaying(!isPlaying) }} ref={videoRef} muted={muted} poster={poster} src={src} contextMenu="false" controlsList="nodownload" className="h-full w-full video_nonFullScr"></video>
+                <video onLoadedData={() => { setVideoLoading(false) }} onLoadStart={() => { setVideoLoading(true) }} onContextMenu={(event) => { event.preventDefault(); }} onDoubleClick={() => setFullScr(!fullScr)} onClick={() => { setPlaying(!isPlaying) }} ref={videoRef} muted={muted} poster={poster} src={src} contextMenu="false" controlsList="nodownload" className="h-full w-full video_nonFullScr"></video>
 
                 <div className={" absolute z-40 top-10 left-1/2 -translate-x-[50%] -translate-y-[50%] transition-all ease-soft-spring duration-500 " + (isSeeking ? "opacity-100 scale-100 visible" : "opacity-0 scale-150 invisible")}>
                     <h3 className="text-2xl rounded-full bg-primary/40 backdrop-blur-sm px-2 py-1 font-bold text-background  ">{getVideoDuration(length)}</h3>
                 </div>
 
+                {isVideoLoading && <div className={" bg-default-300/40 backdrop-blur-sm px-2 py-2 w-20 h-20 flex justify-center items-center absolute z-40 top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] transition-all ease-soft-spring duration-500 "}>
+                    <h3 className="text-2xl  font-bold text-background  ">
+                        <CircularProgress color="danger" />
+                    </h3>
+                </div>}
 
             </div>
 

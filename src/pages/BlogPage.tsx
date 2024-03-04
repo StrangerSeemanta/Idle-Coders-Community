ad2eec103fe84c7134fd6cbd522152975b817e1d
Seemanta Sarker <91196514+StrangerSeemanta@users.noreply.github.com>
2024-03-05 00:32:55 +0600

    Account Pages Added And Simulations

--- src/pages/BlogPage.tsx
+++ src/pages/BlogPage.tsx
@@ -26,7 +26,7 @@ function BlogPage() {
             </div> */}
             <div className="flex flex-col gap-y-5 justify-center items-center h-[80vh] w-full">
                 <h1 className='text-4xl font-bold'>
-                    This Page Is Coming Very Soon!!!
+                    Log In First To See The Blogs
                 </h1>
                 <h1 className='text-xl'>
                     Stay Tuned &amp; Stay With <span className='text-danger'>Idle Coders</span>
